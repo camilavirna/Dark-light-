c393ff53aa51149b69f170d92b66c2503dcce88c
camilavirna <camilavirna_@hotmail.com>
2021-09-21 13:59:33 -0300

    first commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div `
+    height: 60px;
+    background: #272727;
+    color: #FFF;
+    display: flex;
+    align-items: center;
+    padding: 0 30px;
+`;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { 
+    title: 'light',
+
+    colors: {
+        primary: '#FF6EC7',
+        secundary: '#C80E80',
+
+        background: '#f5f5f5',
+        text: '#333'
+    }, 
+};
